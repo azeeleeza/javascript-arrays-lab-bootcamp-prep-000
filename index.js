e34cdedd3cbe004a7c5c16d71d6caee1210ee52c
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 05:14:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name){
+kittens.push(name);
+} //appends a kitten to the end of the kittens array
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+} //prepends a kitten to the beginning of the kittens array
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+} //removes the last kitten from the kittens array
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+} //removes the First kitten from the kittens array
+function appendKitten(name){
+  var newKittens = kittens
+  newKittens.push(name)
+  return newKittens
+} //appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function prependKitten(name){
+  var newKittens = kittens
+  newKittens.unshift(name)
+  return newKittens
+} //prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged
+function removeLastKitten(){
+  var newKittens = kittens
+  newKittens.pop()
+  return newKittens
+} //removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged
+function removeFirstKitten(){
+var newKittens = kittens
+newKittens.shift()
+return newKittens
+} //removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged
