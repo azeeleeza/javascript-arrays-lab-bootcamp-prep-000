36012c21ce0ca77a39ceca2781130a5c76280627
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 05:15:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-
+function destructivelyAppendKitten(name){
+kittens.push(name);
+} //appends a kitten to the end of the kittens array
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+} //prepends a kitten to the beginning of the kittens array
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+} //removes the last kitten from the kittens array
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+} //removes the First kitten from the kittens array
 function appendKitten(name){
   var newKittens = kittens
   newKittens.push(name)
